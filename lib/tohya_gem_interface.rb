cd49baa125edcff274b56f32ead6e6b3ab59c162
ryu <ryu@tohya.net>
2020-07-30 12:13:20 +0200

    new method

--- lib/tohya_gem_interface.rb
+++ lib/tohya_gem_interface.rb
@@ -2,6 +2,19 @@ class TohyaGemInterface
 
   private
 
+  def gem_interface_test
+    puts "gem integration successful"
+  end
+
+  def check_command(list, cmd)
+    if cmd.nil? || !(list.include?(cmd))
+      puts "Missing or Invalid Command."
+      puts "Legal commands are:"
+      list.each { |i| puts "- #{i}" }
+      quit
+    end
+  end
+
   def quit
     exit
   end
