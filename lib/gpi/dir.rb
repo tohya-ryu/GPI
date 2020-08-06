2b916a6ceb1cbd4dbcc6d7946204f2070243e733
= <=>
2020-08-06 13:52:14 +0200

    fix namespace issue

--- lib/gpi/dir.rb
+++ lib/gpi/dir.rb
@@ -3,13 +3,13 @@ module GPI
 class Dir
 
   def self.directories(dir) 
-    Dir.entries(dir).select do |f|
+    ::Dir.entries(dir).select do |f|
       File.directory?(File.join(dir,f)) && f != "." && f != ".."
     end
   end
 
   def self.files(dir)
-    Dir.entries(dir).select do |f|
+    ::Dir.entries(dir).select do |f|
       !(File.directory?(File.join(dir,f)))
     end
   end
