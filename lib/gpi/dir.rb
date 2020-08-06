fd57ed4b0875e4fc4d1f8ca1ccae4021689c5efd
= <=>
2020-08-06 13:41:17 +0200

    new function to retrieve files of dir

--- lib/gpi/dir.rb
+++ lib/gpi/dir.rb
@@ -1,8 +1,14 @@
 class Dir
 
-  def Dir.get_dir(dir) 
+  def self.directories(dir) 
     Dir.entries(dir).select do |f|
       File.directory?(File.join(dir,f)) && f != "." && f != ".."
+    end
+  end
+
+  def self.files(dir)
+    Dir.entries(dir).select do |f|
+      !(File.directory?(File.join(dir,f)))
     end
   end
 
